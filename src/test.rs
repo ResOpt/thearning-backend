b7ffc57258aadd6e34e4674705f81f6a857bab06
Rahman Hakim <rahmanhakim2435@gmail.com>
2022-03-03 05:46:29 +0700

    Separating tests, add get data test to tests, update CI

--- src/test.rs
+++ src/test.rs
@@ -7,7 +7,7 @@ mod test {
     use self::diesel::prelude::*;
 
     use std::io::Read;
-    use rocket::http::{ContentType, Status};
+    use rocket::http::{ContentType, Status, Header};
     use rocket::local::blocking::Client;
     use rocket::serde::json::Json;
     use rocket::serde::json::serde_json::json;
@@ -22,14 +22,45 @@ mod test {
     use crate::db::{self, database_url};
 
 
-    #[derive(Deserialize, Debug)]
+    #[derive(Deserialize)]
     struct Task {
         status: i32,
         token: String,
     }
 
+
+    #[derive(Deserialize)]
+    struct UserData {
+        user_id: String,
+        fullname: String,
+        email: String,
+        status: String,
+        profile_photo: String,
+        bio: String,
+    }
+
+    #[derive(Deserialize)]
+    struct UserDataResponse {
+        status: i32,
+        data: UserData,
+    }
+
+    fn auth_request() -> Task {
+
+        // Construct the client
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+
+        // Authenticating the dummy user
+        let mut response_auth = client.post("/api/auth")
+            .header(ContentType::JSON)
+            .body(r#"{"key":"123", "password":"dummy"}"#).dispatch();
+
+        response_auth.into_json::<Task>().unwrap()
+
+    }
+
     #[test]
-    fn create_and_auth_test() {
+    fn t_1_create_user() {
 
         let string = r#"{
                          "user_id": "123", 
@@ -53,15 +84,43 @@ mod test {
         // Is Response ok?
         assert_eq!(response_create.status(), Status::Ok);
 
-        // Authenticating the dummy user
-        let mut response_auth = client.post("/api/auth")
-            .header(ContentType::JSON)
-            .body(r#"{"key":"123", "password":"dummy"}"#).dispatch();
+    }
 
-        let r = response_auth.into_json::<Task>().unwrap();
+    #[test]
+    fn t_2_auth() {
+        
+        // Getting the auth response
+        let r = auth_request();
 
         // Is status equals to 200?
         assert_eq!(r.status, 200);
+
+    }
+
+    #[test]
+    fn t_3_get_data() {
+        
+        // Construct the client
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+       
+        // Getting the auth response
+        let r = auth_request();
+
+        // Sending GET method to get data with token authentication
+        let mut response_data = client.get("/api/user")
+            .header(Header::new("Authentication", r.token))
+            .dispatch();
+
+        // Deserializing data
+        let r_2 = response_data.into_json::<UserDataResponse>().unwrap();
+
+        // Is user_id equals to 123?
+        assert_eq!(r_2.data.user_id, "123");
+
+    }
+
+    #[test]
+    fn t_4_delete_user() {
 
         // Database connection
         let db_conn = PgConnection::establish(&database_url()).unwrap();
@@ -73,6 +132,7 @@ mod test {
 
         // Is the row deleted?
         assert_eq!(Ok(1), deleted_rows);
+
     }
 
 }
