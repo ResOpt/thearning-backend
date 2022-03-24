f7fb52b338ad5b2da1fa84d89e8286dab30c9419
Rahman Hakim <rahmanhakim2435@gmail.com>
2022-03-24 20:45:29 +0700

    Update files schema

--- migrations/2022-02-14-130600_files/up.sql
+++ migrations/2022-02-14-130600_files/up.sql
@@ -4,6 +4,6 @@ CREATE TABLE files(
     file_id VARCHAR NOT NULL PRIMARY KEY,
     filename VARCHAR NOT NULL,
     file_path VARCHAR NOT NULL,
-    file_urls VARCHAR NOT NULL,
+    file_url VARCHAR NOT NULL,
     filetype VARCHAR NOT NULL
 )
