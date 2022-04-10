e1978f9f605945ff02aa2d8559930eb571d14a00
Rahman Hakim <rahmanhakim2435@gmail.com>
2022-04-11 00:44:18 +0700

    Update migrate script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/bash
 
-for i in {0..13}
+source "./.env"
+
+for i in {0..12}
 do
 	diesel migration revert
+	diesel migration revert --database-url $DATABASE_URL_TEST
 done
 
 diesel migration run
+diesel migration run --database-url $DATABASE_URL_TEST
+cargo test -- --test-threads 1
