dfb381c3ec4b059ff74c231d47661f5b6fc32eb8
Rahman Hakim <rahmanhakim2435@protonmail.com>
2022-05-12 21:39:36 +0700

    Update migrations

--- migrations/2022-04-09-165536_private_comments/up.sql
+++ migrations/2022-04-09-165536_private_comments/up.sql
@@ -1,12 +1,10 @@
 CREATE TABLE private_comments(
      id VARCHAR NOT NULL PRIMARY KEY,
      user_id VARCHAR NOT NULL,
-     assignment_id VARCHAR,
-     announcement_id VARCHAR,
+     submission_id VARCHAR,
      body TEXT NOT NULL,
      created_at TIMESTAMP NOT NULL,
 
      FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-     FOREIGN KEY (assignment_id) REFERENCES assignments(assignment_id) ON DELETE CASCADE,
-     FOREIGN KEY (announcement_id) REFERENCES announcements(announcement_id) ON DELETE CASCADE
+     FOREIGN KEY (submission_id) REFERENCES submissions(submission_id) ON DELETE CASCADE
 )
